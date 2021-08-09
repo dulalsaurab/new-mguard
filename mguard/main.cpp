b0d34dccd688b312ff3df057a836713de5359520
Adam Thieme <athieme@memphis.edu>
2021-08-09 15:47:16 -0500

    added available_streams input small naming changes

--- mguard/main.cpp
+++ mguard/main.cpp
@@ -2,22 +2,22 @@
 #include "parser.cpp"
 
 int main(int argc, char** argv){
-    std::string configFileName("default.policy");
+    std::string configFilePath("default.policy"), availableStreamsFilePath ("policies/available_streams");
 
-    // only allowing -f configFileName to change policy file name
+    // only allowing -f configFilePath to change policy file name
     // very crude way of doing this, but it works for now
     if (argc == 3) {
         std::string s = "-f";
         if ((s.compare(argv[1])) == 0) {
-            configFileName = argv[2];
+            configFilePath = argv[2];
         } else {
             return -1;
         }
     }
 
-    mguard::PolicyParser pp(configFileName);
+    mguard::PolicyParser pp(configFilePath, availableStreamsFilePath);
 
-    if (!pp.processFile()){
+    if (!pp.processFiles()){
         return -1;
     }
 
