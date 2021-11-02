6c861980fba9ce0ba9dd525be4a58425fa96c2ae
Adam Thieme <athieme@memphis.edu>
2021-11-01 21:40:16 -0500

    configFilePath is now PolicyFilePath

--- mguard/main.cpp
+++ mguard/main.cpp
@@ -4,14 +4,14 @@
 // todo: make default.policy the default one unless given other arguments
 // todo: possibly change stream-name to data-stream
 int main(int argc, char** argv){
-    std::string configFilePath("default.policy"), availableStreamsFilePath ("policies/available_streams");
+    std::string policyFilePath("default.policy"), availableStreamsFilePath ("policies/available_streams");
 
-    // only allowing -f configFilePath to change policy file name
+    // only allowing -f policyFilePath to change policy file name
     // very crude way of doing this, but it works for now
     if (argc == 3) {
         std::string s = "-f";
         if ((s.compare(argv[1])) == 0) {
-            configFilePath = argv[2];
+            policyFilePath = argv[2];
         } else {
             return -1;
         }
@@ -19,7 +19,7 @@ int main(int argc, char** argv){
 
     // this createsa a parser with the path to the config file and the available_streams file
     // the resulting ABE policy is automatically generated
-    mguard::PolicyParser pp(configFilePath, availableStreamsFilePath);
+    mguard::PolicyParser pp(policyFilePath, availableStreamsFilePath);
 
 //    // this is just for testing purposes
 //    std::cout << pp << std::endl;
