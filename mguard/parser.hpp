a414cd85d10fd86a00a8ab8ca3ca97186044e1fa
Adam Thieme <athieme@memphis.edu>
2022-01-07 03:36:03 -0600

    remove unused methods and clean up formatting

--- mguard/parser.hpp
+++ mguard/parser.hpp
@@ -1,9 +1,10 @@
 #include <boost/algorithm/string.hpp>
 #include <boost/property_tree/ptree.hpp>
 #include <ostream>
-//#include "attributeFilter.hpp"
 
 namespace mguard {
+    using ConfigSection = boost::property_tree::ptree;
+
     class attributeFilter {
     private:
         bool isAllowed;
@@ -14,34 +15,31 @@ namespace mguard {
 
         std::string attribute;
     };
-    using ConfigSection = boost::property_tree::ptree;
+
     class PolicyParser {
     public:
         PolicyParser(std::basic_string<char> configFilePath, std::basic_string<char> availableStreams);
 
         bool parseFiles();
 
-        static std::list<std::string> split(const std::string& basicString, const std::string& delimeter);
-
-        static std::string trim(const std::string& basicString);
-
-        static std::string doStringThing(const std::list<std::string>& list, const std::string& operation);
+        bool generateABEPolicy();
 
         friend std::ostream &operator<<(std::ostream &os, const PolicyParser &parser);
 
-        const std::string &getABEPolicy() const;
-
-        static std::list<std::string> splitRequesters(const std::string& basicString);
 
     private:
+        static std::list<std::string> splitRequesters(const std::string& basicString);
+
+        static std::string doStringThing(const std::list<std::string>& list, const std::string& operation);
+
+        static std::list<std::string> split(const std::string& basicString, const std::string& delimeter);
+
         bool parseAvailableStreams(std::istream &input);
         std::list<std::string> availableStreamLevels, availableStreams, allowedRequesters, availableAttributes;
 
         bool parsePolicy(std::istream &input);
 
         bool processAttributeFilter(ConfigSection &section, bool isAllowed);
-
-        bool generateABEPolicy();
 
         // full path of the config/policy file
         std::string configFilePath, availableStreamsPath;
