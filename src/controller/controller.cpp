541ea0ff2ac0ffaba9bff1aeb40d32c6a8df83ca
Saurab Dulal <sdulal@memphis.edu>
2022-01-18 19:10:42 -0600

    minor fix

--- src/controller/controller.cpp
+++ src/controller/controller.cpp
@@ -4,7 +4,7 @@ using namespace ndn;
 
 Controller::Controller(const ndn::Name& aaPrefix)
 // TODO: need to fix this
-: m_keyChain("pib-sqlite3:/Users/sdulal/.ndn/", "tpm-file:/Users/sdulal/.ndn/ndnsec-tpm-file")
+: m_keyChain()
 , m_aaCert(m_keyChain.getPib().getIdentity("/mguard/aa").getDefaultKey().getDefaultCertificate())
 , m_attrAuthority(m_aaCert, m_face, m_keyChain)
 {
