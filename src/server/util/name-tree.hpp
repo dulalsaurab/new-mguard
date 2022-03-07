4dbc09fb68f5255848933433de7b31a4d23e4e5f
Suravi Regmi <regmisuravi@gmail.com>
2022-03-07 15:45:13 -0600

    Add name-tree hpp file

--- src/server/util/name-tree.hpp
+++ src/server/util/name-tree.hpp
@@ -2,41 +2,69 @@
 #include <string>
 #include "../common.hpp"
 
+#define NDN_NAME_COMPONENT_BLOCK_SIZE 38
+typedef uint16_t ndn_table_id_t;
+
+
 namespace mguard {
 namespace util {
 
-struct node
-{
- std::string name;
- node* right;
- node* left;
-};
+
+typedef struct nametree_entry{
+  uint8_t val[NDN_NAME_COMPONENT_BLOCK_SIZE];
+  struct nametree_entry* sub; /// Subtree
+  struct nametree_entry* cop[2]; /// Child or parent
+  ndn_table_id_t pit_id;
+  ndn_table_id_t fib_id;
+} nametree_entry_t;
+
+
+typedef struct ndn_nametree{
+  nametree_entry_t *nil, *root;
+  nametree_entry_t pool[];
+}ndn_nametree_t;
+
 
 class NameTree()
 {
-public:
- NameTree();
+  public:
 
- void
- insert(ndn::Name prefix);
+  public:
+  NameTree(uint8_t val[NDN_NAME_COMPONENT_BLOCK_SIZE],
+            ndn_table_id_t left_child,
+            ndn_table_id_t right_bro,
+            ndn_table_id_t pit_id,
+            ndn_table_id_t cs_id,
+            ndn_table_id_t fib_i
+            );
 
-//  void
-//  delete(ndn::Name prefix);
 
-//  void
-//  search(ndn::Name prefix);
+  typedef nametree_entry_t ndn_nametree_t[];
 
-//  void
-//  longestPrefixMatch();
+  #define NDN_NAMETREE_RESERVE_SIZE(entry_count) (sizeof(nametree_entry_t) * (entry_count))
 
-//  void
-//  isLeaf(ndn::Name prefix);
+  void
+  ndn_nametree_init(void* memory, ndn_table_id_t capacity);
 
-//  void
-//  getAllchildren(nan::Name prefix);
+  nametree_entry_t*
+  ndn_nametree_find_or_insert(ndn_nametree_t* nametree, uint8_t name[], size_t len);
 
-private:
- node* root;
+  nametree_entry_t*
+  ndn_nametree_prefix_match(
+    ndn_nametree_t* nametree,
+    uint8_t name[],
+    size_t len,
+    enum NDN_NAMETREE_ENTRY_TYPE type);
+
+  nametree_entry_t*
+  ndn_nametree_find(ndn_nametree_t *nametree, uint8_t name[], size_t len);
+
+  nametree_entry_t*
+  ndn_nametree_at(ndn_nametree_t *self, ndn_table_id_t id);
+
+  ndn_table_id_t
+  ndn_nametree_getid(ndn_nametree_t *self, nametree_entry_t* entry);
+
 }
 
 } // util
