82d3e1ddba1a2b482d44933b8cdc1a4ba0113816
Adam Thieme <athieme@memphis.edu>
2022-05-09 21:31:25 -0500

    adding /ndn/ and /edu/

--- quick_install.sh
+++ quick_install.sh
@@ -6,16 +6,16 @@ sudo ldconfig
 # Creating Identifies/Certificates
 
 # publisher
-ndnsec key-gen -t r /org/md2k
+ndnsec key-gen -t r /ndn/org/md2k
 # controller
-ndnsec key-gen -t r /mguard/controller
+ndnsec key-gen -t r /ndn/edu/mguard/controller
 # Consumer
-ndnsec key-gen -t r /org/md2k/A
+ndnsec key-gen -t r /ndn/org/md2k/A
 # Attribute Authority
-ndnsec key-gen -t r /mguard/aa
+ndnsec key-gen -t r /ndn/edu/mguard/aa
 
 # md2k data generator
-cd data-generator
+cd data-generator || exit
 git clone https://github.com/MD2Korg/CerebralCortex-Random-Data-Generator
 mv CerebralCortex-Random-Data-Generator CerebralCortexRandomDataGenerator
 pip install findspark
