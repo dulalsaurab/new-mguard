92a26f9d0aa52650fa586307724904c12303d679
Adam Thieme <athieme@memphis.edu>
2022-05-09 20:18:48 -0500

    add installation for data generation (testing only)

--- quick_install.sh
+++ quick_install.sh
@@ -13,3 +13,12 @@ ndnsec key-gen -t r /mguard/controller
 ndnsec key-gen -t r /org/md2k/A
 # Attribute Authority
 ndnsec key-gen -t r /mguard/aa
+
+# md2k data generator
+cd data-generator
+git clone https://github.com/MD2Korg/CerebralCortex-Random-Data-Generator
+mv CerebralCortex-Random-Data-Generator CerebralCortexRandomDataGenerator
+pip install findspark
+pip install cerebralcortex-kernel
+sudo apt install default-jdk
+cd ..
