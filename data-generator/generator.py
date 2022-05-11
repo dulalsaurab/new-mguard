034077b45882e9f20420f8f5b1985bb627a2c5ea
suraviregmi <regmisuravi@gmail.com>
2022-05-11 21:29:04 +0000

    reset folder before generating data

--- data-generator/generator.py
+++ data-generator/generator.py
@@ -28,11 +28,11 @@ def get_cc(start_time='2022-05-08 21:08:10', end_time='2022-05-08 21:08:10'):
 
     CC = Kernel(cc_configs="default", study_name=study_name, new_study=True)
 
-    battery_stream_name = "ndn--org--md2k--{}--{}--battery--phone".format(study_name,user_id)
-    location_stream_name = "ndn--org--md2k--{}--{}--gps--phone".format(study_name,user_id)
+    battery_stream_name = "ndn--org--md2k--{}--{}--phone--battery".format(study_name,user_id)
+    location_stream_name = "ndn--org--md2k--{}--{}--phone--gps".format(study_name,user_id)
     semantic_location_stream_name = "ndn--org--md2k--{}--{}--data_analysis--gps_episodes_and_semantic_location".format(study_name,user_id)
-    accel_stream_name = "ndn--org--md2k--{}--{}--accelerometer--phone".format(study_name,user_id)
-    gyro_stream_name = "ndn--org--md2k--{}--{}--gyroscope--phone".format(study_name,user_id)
+    accel_stream_name = "ndn--org--md2k--{}--{}--phone--accelerometer".format(study_name,user_id)
+    gyro_stream_name = "ndn--org--md2k--{}--{}--phone--gyroscope".format(study_name,user_id)
 
     gen_battery_data(CC, study_name=study_name, user_id=user_id, stream_name=battery_stream_name, start_time=start_time, end_time=end_time)
     gen_location_datastream(CC, study_name=study_name, user_id=user_id, stream_name=location_stream_name, start_time=start_time, end_time=end_time)
