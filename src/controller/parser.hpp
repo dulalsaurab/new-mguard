9ec97f13d1d05f53be1dec8880008975f12a92f3
Adam Thieme <athieme@memphis.edu>
2022-06-20 22:08:07 -0500

    more consolidating of available streams parsing

--- src/controller/parser.hpp
+++ src/controller/parser.hpp
@@ -32,14 +32,14 @@ class PolicyParser
 public:
   explicit PolicyParser(std::basic_string<char> availableStreams);
 
+  bool
+  parseAvailableStreams(const std::basic_string<char>& streamsFilePath);
+
   PolicyDetail
   parsePolicy(const std::basic_string<char>& policyFilePath);
 
-  void
-  inputStreams(const std::basic_string<char>& streamsFilePath);
-
-    static std::pair<std::string, std::string>
-    parseAttribute(std::string attribute);
+  static std::pair<std::string, std::string>
+  parseAttribute(std::string attribute);
 
 private:
   bool
@@ -54,8 +54,6 @@ private:
   static std::list<std::string> 
   split(const std::string& basicString, const std::string& delimeter);
 
-  bool 
-  parseAvailableStreams(const std::basic_string<char>& streamsFilePath);
 
   void
   parsePolicy(std::istream &input);
