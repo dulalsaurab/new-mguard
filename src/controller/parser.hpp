3adb2480cffbc1809b41723f72833e3cff0999cf
Adam Thieme <athieme@memphis.edu>
2022-06-20 21:26:40 -0500

    rename inputPolicy to parsePolicy

--- src/controller/parser.hpp
+++ src/controller/parser.hpp
@@ -33,7 +33,7 @@ public:
   explicit PolicyParser(std::basic_string<char> availableStreams);
 
   PolicyDetail
-  inputPolicy(const std::basic_string<char>& policyFilePath);
+  parsePolicy(const std::basic_string<char>& policyFilePath);
 
   void
   inputStreams(const std::basic_string<char>& streamsFilePath);
