823191f7162316c19c65b85d917b48dae05e5b78
Adam Thieme <athieme@memphis.edu>
2022-07-04 22:25:23 -0500

    sandbox for cmake testing

---
+++ sandbox/sandbox.cpp
@@ -0,0 +1,7 @@
+#include "iostream"
+#include "../src/server/util/name-tree.cpp"
+int main(){
+    std::cout << "hello world" << std::endl;
+    mguard::util::nametree::NameTree nameTree;
+    return 0;
+}
