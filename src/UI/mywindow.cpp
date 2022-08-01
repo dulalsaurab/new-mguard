0064f0655a4a24b2d6df9caddc7ad52bc2bbb9f7
suraviregmi <regmisuravi@gmail.com>
2022-08-01 18:23:07 +0000

    add ui compoments tp consumer app

---
+++ src/UI/mywindow.cpp
@@ -0,0 +1,418 @@
+
+#include <user/subscriber.hpp>
+#include <common.hpp>
+
+#include <iostream>
+#include "mywindow.hpp"
+#include <fstream> // std::fstream
+#include <string>
+#include <sstream>
+#include <algorithm>
+
+#include <ndn-cxx/util/logger.hpp>
+#include <ndn-cxx/face.hpp>
+
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/parsers.hpp>
+
+using namespace ndn::time_literals;
+
+NDN_LOG_INIT(mguard.ui);
+
+mywindow::mywindow(ndn::Name &consumerPrefix, ndn::Name &syncPrefix, ndn::Name &controllerPrefix,
+                   std::string &consumerCertPath, std::string &aaCertPath)
+    : m_subscriber(consumerPrefix, syncPrefix, controllerPrefix,
+                   consumerCertPath, aaCertPath, 1000_ms,
+                   std::bind(&mywindow::processDataCallback, this, _1),
+                   std::bind(&mywindow::processSubscriptionCallback, this, _1))
+{
+
+    mywindow::handler();
+}
+
+mywindow::~mywindow()
+{
+}
+
+void mywindow::on_quit_click()
+{
+
+    Gtk::MessageDialog quit_dialog("Are you sure?", false,
+                                   Gtk::MESSAGE_ERROR,
+                                   Gtk::BUTTONS_YES_NO,
+                                   true);
+    quit_dialog.set_title("Quit");
+    int result = quit_dialog.run();
+    switch (result)
+    {
+    case (yes_response):
+        hide();
+        break;
+    case (no_response):
+        quit_dialog.hide();
+        break;
+    }
+}
+
+// convert file content to string
+std::string mywindow::file_to_string(std::string filename)
+{
+    //     std::cout<<"set detail text function called" << std::endl;
+    //         std::cout<< filename << std::endl;
+
+    // //
+    //     std::ifstream input(filename);
+    //     std::stringstream sstr;
+
+    //     while(input >> sstr.rdbuf());
+
+    //     // std::cout << sstr.str() << std::endl;
+    //     std::string wholefile = sstr.str();
+    return "true";
+}
+
+void mywindow::on_changed(Glib::RefPtr<Gtk::TreeSelection> c)
+{
+
+    std::cout << "on changed function" << std::endl;
+    Gtk::TreeModel::iterator iter = c->get_selected();
+    if (iter) // If anything is selected
+    {
+        Gtk::TreeModel::Row row = *iter;
+        std::string id = row.get_value(m_Columns.m_id);
+
+        std::stringstream filename;
+        filename << "content" << id << ".txt";
+        std::string whole_file = mywindow::file_to_string(filename.str());
+
+        ss_detail->get_buffer()->set_text(whole_file);
+    }
+}
+
+void mywindow::on_row(std::string b)
+{
+
+    // if not in list add else remove from list
+    if (std::find(sub_st.begin(), sub_st.end(), b) == sub_st.end())
+    {
+        sub_st.push_back(b);
+    }
+    else
+    {
+        sub_st.erase(std::remove(sub_st.begin(), sub_st.end(), b), sub_st.end());
+    }
+    mywindow::change_btn_display();
+    std::cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@2" << '\n';
+}
+
+void mywindow::change_btn_display()
+{
+
+    //  Gtk::Widget * const parent = gtk_widget_get_parent(GTK_WIDGET(as_grid));
+    std::vector<Gtk::Widget *> children = as_grid->get_children();
+
+    // as_grid->remove(children);
+    for (long unsigned int a = 0; a < children.size(); a++)
+    {
+        std::cout << "cjild" << std::endl;
+        as_grid->remove(*children[a]);
+    }
+
+    for (long unsigned int s = 0; s < acc_st.size(); s++)
+    {
+
+        Gtk::Label *stream_name = Gtk::manage(new Gtk::Label());
+        stream_name->set_markup(acc_st[s]);
+        as_grid->attach(*stream_name, 1, s, 1, 1);
+
+        Gtk::Button *button = Gtk::manage(new Gtk::Button());
+        Glib::RefPtr<Gtk::CssProvider> css_provider = Gtk::CssProvider::create();
+        if (std::find(sub_st.begin(), sub_st.end(), acc_st[s]) == sub_st.end())
+        {
+
+            // Element in subscribed vector.
+            button->set_label("subscribe");
+            css_provider->load_from_data(
+                "button {background-image: image(#bcffb5); margin-top: 10px;  margin-left: 10px;}");
+            button->get_style_context()->add_provider(
+                css_provider, GTK_STYLE_PROVIDER_PRIORITY_USER);
+        }
+        else
+        {
+
+            button->set_label("unsubscribe");
+            css_provider->load_from_data(
+                "button {background-image: image(#ffc2b5);  margin-top: 10px;  margin-left: 10px;}");
+            button->get_style_context()->add_provider(
+                css_provider, GTK_STYLE_PROVIDER_PRIORITY_USER);
+        }
+        as_grid->attach(*button, 2, s, 1, 1);
+        button->signal_clicked().connect(sigc::bind(sigc::mem_fun(*this, &mywindow::on_row), acc_st[s]));
+    }
+
+    show_all_children();
+}
+
+// void mywindow::on_row( Glib::RefPtr<Gtk::Button> b) {
+//     display_btn = Glib::RefPtr<Gtk::Button>::cast_dynamic(Gtk::Builder->get_object(b));
+//     // Gtk::Button* button = Glib::wrap(b);
+//     //   b->set_label("subscribe");
+//     std::cout <<display_btn.get_label() << '\n';
+// }
+
+void mywindow::processDataCallback(const std::vector<std::string> &updates)
+{
+    // print the data being shown here
+    for (auto &a : updates)
+        std::cout << a << std::endl;
+}
+  void
+  mywindow::handler()
+  {
+    // mywindow::show_ui();
+    m_subscriber.run();
+  }
+
+  void mywindow::show_ui(){
+    Glib::RefPtr<Gtk::Builder> ui = Gtk::Builder::create_from_file("src/UI/tabsbox.glade");
+    std::cout << "hell" << std::endl;
+
+    // std::cout << consumerPrefix << std::endl;
+
+    if (ui)
+    {
+
+        ui->get_widget<Gtk::Box>("box", box);
+        std::cout << "box" << std::endl;
+
+        ss_tv = Glib::RefPtr<Gtk::TreeView>::cast_dynamic(
+            ui->get_object("ss_tv"));
+        std::cout << "glib error" << std::endl;
+
+        policy = Glib::RefPtr<Gtk::Label>::cast_dynamic(
+            ui->get_object("policy"));
+        list_store = Glib::RefPtr<Gtk::ListStore>::cast_dynamic(
+            ui->get_object("list_store"));
+        ss_detail = Glib::RefPtr<Gtk::TextView>::cast_dynamic(
+            ui->get_object("ss_detail"));
+
+        acc_st = {"/ndn/org/md2k/mguard/dd40c/phone/accelerometer",
+                  "/ndn/org/md2k/mguard/dd40c/data_analysis/gps_episodes_and_semantic_location ",
+                  "/ndn/org/md2k/mguard/dd40c/phone/gyroscope",
+                  "/ndn/org/md2k/mguard/dd40c/phone/battery",
+                  "/ndn/org/md2k/mguard/dd40c/phone/gps"};
+        sub_st = {"/ndn/org/md2k/mguard/dd40c/phone/accelerometer",
+                  "/ndn/org/md2k/mguard/dd40c/phone/gyroscope",
+                  "/ndn/org/md2k/mguard/dd40c/phone/gps"};
+
+        policy->set_text("Your Policy : /ndn/org/md2k/mguard/dd40c/phone/accelerometer");
+
+        list_store = Gtk::ListStore::create(m_Columns);
+        ss_tv->set_model(list_store);
+        ss_tv->set_reorderable();
+        ss_tv->set_headers_clickable(true);
+        ss_tv->set_headers_visible(true);
+
+        // #Fill the list_store model
+        auto row = *(list_store->append());
+        row[m_Columns.m_id] = "1";
+        row[m_Columns.m_timestamp] = "02/11/1998";
+        row[m_Columns.m_source] = "/ndn/org/md2k/mguard/dd40c/phone/accelerometer";
+        row[m_Columns.m_info] = "1654907929.430571 DEBUG: [mguard.subscriber] ...";
+
+        // #Fill the list_store model
+        row = *(list_store->append());
+        row[m_Columns.m_id] = "2";
+        row[m_Columns.m_timestamp] = "12/12/2021";
+        row[m_Columns.m_source] = "/ndn/org/md2k/mguard/dd40c/phone/gyroscope";
+        row[m_Columns.m_info] = "1654907929.430571 DEBUG: [mguard.subscriber] ...";
+
+        // #Fill the list_store model
+        row = *(list_store->append());
+        row[m_Columns.m_id] = "3";
+        row[m_Columns.m_timestamp] = "03/02/2020";
+        row[m_Columns.m_source] = "/ndn/org/md2k/mguard/dd40c/phone/gps";
+        row[m_Columns.m_info] = "1654907929.430571 DEBUG: [mguard.subscriber] ...";
+
+        // Add the TreeView's view columns:
+        ss_tv->append_column("id", m_Columns.m_id);
+        ss_tv->append_column("timestamp", m_Columns.m_timestamp);
+        ss_tv->append_column("source", m_Columns.m_source);
+        ss_tv->append_column("info", m_Columns.m_info);
+
+        m_refTreeSelection = ss_tv->get_selection();
+        // m_refTreeSelection->signal_changed().connect(sigc::mem_fun(*this, &mywindow::on_changed));
+        m_refTreeSelection->signal_changed().connect(sigc::bind(sigc::mem_fun(*this, &mywindow::on_changed), m_refTreeSelection));
+
+        //   m_button1.signal_clicked().connect(sigc::bind(sigc::mem_fun(*this, &HelloWorld::on_button_clicked), "button 1"));
+        //    Gtk::TreeModel::iterator iter = m_refTreeSelection->get_selected();
+        //     if(iter) //If anything is selected
+        //     {
+        //     Gtk::TreeModel::Row row = *iter;
+        //     std::cout<< iter;
+        //     //Do something with the row.
+        //     }
+
+        // Text box to show the conteot of each stream
+        Glib::RefPtr<Gtk::TextBuffer> m_refTextBuffer1 = Gtk::TextBuffer::create();
+        // Fill text box with file content
+        std::string whole_file = mywindow::file_to_string("ctrue");
+        m_refTextBuffer1->set_text(whole_file);
+        ss_detail->set_buffer(m_refTextBuffer1);
+
+        as_grid = Glib::RefPtr<Gtk::Grid>::cast_dynamic(
+            ui->get_object("as_grid"));
+        mywindow::change_btn_display();
+
+        std::cout << "Hello" << std::endl;
+
+        add(*box);
+    }
+
+    set_title("MGuard");
+    set_default_size(400, 400);
+    show_all();
+    std::cout << "Shows everything" << std::endl;
+
+
+  }
+
+
+void mywindow::update_available_streams_view(std::unordered_set<ndn::Name> streams){
+// //  Gtk::Widget * const parent = gtk_widget_get_parent(GTK_WIDGET(as_grid));
+//     std::vector<Gtk::Widget *> children = as_grid->get_children();
+//     // std::vector<ndn::Name> availableStreams;
+//     // as_grid->remove(children);
+//     for (long unsigned int a = 0; a < children.size(); a++)
+//     {
+//         std::cout << "cjild" << std::endl;
+//         as_grid->remove(*children[a]);
+//     }
+
+//     int counter = 0;
+//     for (auto &a : streams)
+//     {
+//         // std::map<std::string, MyClass*> map;
+
+//     // std::map<ndn::Name, std::string> a;
+//         // const std::map<ndn::Name, std::string>&
+//         std::string fullName ="hello";
+//         // std::string fullName = a.toUri();
+//         Gtk::Label *stream_name = Gtk::manage(new Gtk::Label());
+
+        
+//         stream_name->set_markup("fullName");
+//         as_grid->attach(*stream_name, 1, counter, 1, 1);
+
+//         Gtk::Button *button = Gtk::manage(new Gtk::Button());
+//         Glib::RefPtr<Gtk::CssProvider> css_provider = Gtk::CssProvider::create();
+        
+//         as_grid->attach(*button, 2, counter, 1, 1);
+//         button->signal_clicked().connect(sigc::bind(sigc::mem_fun(*this, &mywindow::on_row), a));
+
+//       NDN_LOG_INFO(++counter << ": " << a);
+//     //   availableStreams.push_back(a);
+//     }
+
+
+    show_all_children();
+
+  }
+
+
+void mywindow::processSubscriptionCallback(const std::unordered_set<ndn::Name> &streams)
+{
+
+       
+
+    // check for convergence.
+    m_subscriber.checkConvergence();
+
+    // stop the process event
+    m_subscriber.stop();
+
+    NDN_LOG_INFO("\n\nStreams available for subscription");
+    std::vector<ndn::Name> availableStreams, subscriptionList;
+    int counter = 0;
+    mywindow::show_ui();
+
+    if (streams.size() <= 0)
+    {
+        //show no stream text
+        Gtk::Label *no_stream = Gtk::manage(new Gtk::Label());
+        no_stream->set_markup("No eligible stream found for your policy");
+        as_grid->attach(*no_stream, 1, 0, 1, 1);
+
+      NDN_LOG_INFO("No eligible stream found for your policy");
+    }
+
+    // mywindow::update_available_streams_view(streams);
+    
+    // print avaliable streams here
+    for (auto &a : streams)
+    {
+        
+
+      NDN_LOG_INFO(++counter << ": " << a);
+      availableStreams.push_back(a);
+    }
+
+    // these codes are only for testing purposes
+    // automatically subscriber to the respective streams
+
+    // A. battery only
+    subscriptionList.push_back(availableStreams[0]); // battery
+
+    // all stream
+    // subscriptionList.push_back(availableStreams[0]); // battery
+    // subscriptionList.push_back(availableStreams[1]); // semloc
+    // subscriptionList.push_back(availableStreams[3]); // gps
+
+    // not gps
+    // subscriptionList.push_back(availableStreams[0]); // battery
+    // subscriptionList.push_back(availableStreams[2]); // sem_loc
+
+    // accelerometer
+    // subscriptionList.push_back(availableStreams[0]); // battery
+
+    // only work
+    // subscriptionList.push_back(availableStreams[0]); // gps, only the one with attribute work should be accessible
+
+    for (auto &s : subscriptionList)
+    {
+      // m_subscriber.subscribe(s);
+      NDN_LOG_DEBUG("Subscribed to the stream/s" << s); // << std::endl;
+    }
+    // uncomment if: taking input from user ----------------------------------------------
+
+    // std::vector<int> input; //
+    // std::cout << "enter selection, enter any char to stop" << std::endl;
+    // while(!std::cin.fail())
+    // {
+    //     int value;
+    //     std::cin >> value;
+    //     if(!std::cin.fail())
+    //       input.push_back(value);
+    // }
+    // std::cout << "\n" << std::endl;
+    // std::cout << "Subscribed to the stream/s" << std::endl;
+    // for (auto k : input)
+    // {
+    //   auto ind = k-1;
+    //   std::cout << k << ": " << availableStreams[ind] << std::endl;
+    //   if (availableStreams[ind] != "/") // todo: fix this
+    //     subscriptionList.push_back(availableStreams[ind]);
+    // }
+
+    // taking input from user end ----------------------------------------------
+
+    m_subscriber.setSubscriptionList(subscriptionList);
+    // run the processevent again, this time with sync as well
+    
+    // rerunning the process event closes the UI
+    // m_subscriber.run(true);
+    // mywindow::show_ui();
+
+    // m_subscriber.run();
+}
---
+++ src/UI/mywindow.hpp
@@ -0,0 +1,95 @@
+#ifndef MYWINDOW_H
+#define MYWINDOW_H
+
+#include <gtkmm.h>
+
+class mywindow : public Gtk::Window
+{
+public:
+  mywindow(ndn::Name &consumerPrefix, ndn::Name &syncPrefix, ndn::Name &controllerPrefix,
+           std::string &consumerCertPath, std::string &aaCertPath); // constructor);
+  virtual ~mywindow();
+  static const int view_subscriber_list = -8;
+  static const int view_accessble_stream = -9;
+  static const int yes_response = -6;
+  static const int no_response = -4;
+  static const int ok_response = -5;
+  ndn::Face m_face;
+  mguard::subscriber::Subscriber m_subscriber;
+
+  std::vector<std::string> acc_st = {};
+  std::vector<std::string> sub_st = {};
+
+  Glib::RefPtr<Gtk::Builder> ui;
+  Gtk::Box *box;
+  Glib::RefPtr<Gtk::ScrolledWindow> window;
+  Glib::RefPtr<Gtk::Fixed> fixed;
+  Glib::RefPtr<Gtk::Stack> stack;
+  Glib::RefPtr<Gtk::Fixed> as_tab;
+  Glib::RefPtr<Gtk::Label> as_label;
+  Glib::RefPtr<Gtk::Label> policy;
+  Glib::RefPtr<Gtk::Fixed> ss_tab;
+
+  Glib::RefPtr<Gtk::Viewport> ss_viewport;
+  Glib::RefPtr<Gtk::TreeView> ss_tv;
+  Glib::RefPtr<Gtk::TextView> ss_detail;
+
+  Glib::RefPtr<Gtk::TreeSelection> select;
+
+  Glib::RefPtr<Gtk::StackSwitcher> switch_s1;
+  Glib::RefPtr<Gtk::Grid> as_grid;
+
+  Glib::RefPtr<Gtk::ListStore> list_store;
+
+  class ModelColumns : public Gtk::TreeModelColumnRecord
+  {
+  public:
+    ModelColumns()
+    {
+      add(m_id);
+      add(m_timestamp);
+      add(m_source);
+      add(m_info);
+    }
+
+    Gtk::TreeModelColumn<std::string> m_id;
+    Gtk::TreeModelColumn<std::string> m_timestamp;
+    Gtk::TreeModelColumn<std::string> m_source;
+    Gtk::TreeModelColumn<std::string> m_info;
+  };
+
+  ModelColumns m_Columns;
+
+  Gtk::TreeModel::Path m_TreePath;
+
+  Gtk::Window *m_pWindow_Example;
+
+  Glib::RefPtr<Gtk::TreeSelection> m_refTreeSelection;
+
+  void on_quit_click();
+  void change_btn_display();
+  // void on_select_changed(Gtk::Widget);
+  void on_changed(Glib::RefPtr<Gtk::TreeSelection>);
+  void show_accessible_stream_from_file();
+  //    static bool select_function(const Glib::RefPtr<Gtk::TreeModel>& model, const Gtk::TreeModel::Path& path, bool currently_selected);
+
+  //   virtual void on_treeview_row_activated(const Gtk::TreeModel::Path& path, Gtk::TreeViewColumn* column);
+
+  //    void button_press_event(GTk::Treeview, event):
+  std::string file_to_string(std::string);
+
+  //    void user_function (Gtk::TreeView   ,
+  //                    Gtk::TreePath     ,
+  //                    Gtk::TreeViewColumn ,
+  //                    gpointer           );
+  void on_row(std::string);
+  void handler();
+  void show_ui();
+  void update_available_streams_view(std::unordered_set<ndn::Name>);
+
+  void processDataCallback(const std::vector<std::string> &updates);
+  void processSubscriptionCallback(const std::unordered_set<ndn::Name> &streams);
+  // void on_row(Glib::RefPtr<Gtk::Button>);
+};
+
+#endif
