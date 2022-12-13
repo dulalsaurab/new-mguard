fe5ccfcfe9705e0aeefcd7625fd53b4a796ffaab
Adam Thieme <athieme@memphis.edu>
2022-12-13 02:58:47 -0600

    initial parser unit test commit

---
+++ tests/unit-tests/parser.t.cpp
@@ -0,0 +1,32 @@
+#include "../test-common.hpp"
+
+#include <controller/parser.hpp>
+
+using namespace ndn;
+
+namespace mguard {
+namespace parser {
+namespace tests {
+
+BOOST_FIXTURE_TEST_SUITE(TestNameTree, mguard::tests::IdentityTimeFixture)
+
+BOOST_AUTO_TEST_CASE(Constructor)
+{
+    PolicyParser parser("available_streams");
+    auto result = parser.parsePolicy("policy1");
+    BOOST_CHECK(result.policyIdentifier == "1");
+    BOOST_CHECK(result.abePolicy == "(/ndn/org/md2k/mguard/dd40c/phone/accelerometer AND /ndn/org/md2k/ATTRIBUTE/location/gym) OR (/ndn/org/md2k/mguard/dd40c/phone/battery AND /ndn/org/md2k/ATTRIBUTE/location/commuting)");
+    for (const auto &stream : result.streams) {
+        BOOST_CHECK(
+                stream == "/ndn/org/md2k/mguard/dd40c/phone/accelerometer" ||
+                stream == "/ndn/org/md2k/mguard/dd40c/phone/battery"
+                );
+    }
+    for (const auto &requester : result.requesters) {
+        BOOST_CHECK(requester == "/ndn/org/md2k/A");
+    }
+}
+BOOST_AUTO_TEST_SUITE_END()
+}
+}
+}
