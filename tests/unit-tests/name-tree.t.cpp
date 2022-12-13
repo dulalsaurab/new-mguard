b8271538c1e1711b838a467fba7642deaf3d4a9e
Adam Thieme <athieme@memphis.edu>
2022-12-13 02:24:12 -0600

    findNode

--- tests/unit-tests/name-tree.t.cpp
+++ tests/unit-tests/name-tree.t.cpp
@@ -66,6 +66,9 @@ BOOST_AUTO_TEST_CASE(Constructor)
     BOOST_CHECK(!nameTree.isChild(nameTree.findNode("/aa/bb"), "/aa/bb/dd"));
 
     // findNode
+    BOOST_CHECK(nameTree.findNode("/aa/bb/cc")->m_fullName == "/aa/bb/cc");
+    BOOST_CHECK(nameTree.findNode("/aa/ff")->m_fullName == "/aa/ff");
+    BOOST_CHECK(nameTree.findNode("/aa/bb/cc/dd") == nullptr);
 
     BOOST_CHECK(true);
 }
