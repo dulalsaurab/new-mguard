18d6d30764cce9f321c7e4e5f8c21d701015a1af
Adam Thieme <athieme@memphis.edu>
2022-12-13 03:05:43 -0600

    parenthesis around the policy

--- tests/unit-tests/parser.t.cpp
+++ tests/unit-tests/parser.t.cpp
@@ -15,7 +15,7 @@ BOOST_AUTO_TEST_CASE(Constructor)
     PolicyParser parser("tests/unit-tests/available_streams");
     auto result = parser.parsePolicy("tests/unit-tests/policy1");
     BOOST_CHECK(result.policyIdentifier == "1");
-    BOOST_CHECK(result.abePolicy == "(/ndn/org/md2k/mguard/dd40c/phone/accelerometer AND /ndn/org/md2k/ATTRIBUTE/location/gym) OR (/ndn/org/md2k/mguard/dd40c/phone/battery AND /ndn/org/md2k/ATTRIBUTE/location/commuting)");
+    BOOST_CHECK(result.abePolicy == "((/ndn/org/md2k/mguard/dd40c/phone/accelerometer AND /ndn/org/md2k/ATTRIBUTE/location/gym) OR (/ndn/org/md2k/mguard/dd40c/phone/battery AND /ndn/org/md2k/ATTRIBUTE/location/commuting))");
     for (const auto &stream : result.streams) {
         BOOST_CHECK(
                 stream == "/ndn/org/md2k/mguard/dd40c/phone/accelerometer" ||
