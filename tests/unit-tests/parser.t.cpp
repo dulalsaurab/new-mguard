be61251131d63a033384e76fafbf93aa17e36838
Adam Thieme <athieme@memphis.edu>
2022-12-13 03:06:55 -0600

    formatting

--- tests/unit-tests/parser.t.cpp
+++ tests/unit-tests/parser.t.cpp
@@ -13,16 +13,18 @@ BOOST_FIXTURE_TEST_SUITE(TestParser, mguard::tests::IdentityTimeFixture)
 BOOST_AUTO_TEST_CASE(Constructor)
 {
     PolicyParser parser("tests/unit-tests/available_streams");
-    auto result = parser.parsePolicy("tests/unit-tests/policy1");
+    PolicyDetail result;
+
+    result = parser.parsePolicy("tests/unit-tests/policy1");
     BOOST_CHECK(result.policyIdentifier == "1");
     BOOST_CHECK(result.abePolicy == "((/ndn/org/md2k/mguard/dd40c/phone/accelerometer AND /ndn/org/md2k/ATTRIBUTE/location/gym) OR (/ndn/org/md2k/mguard/dd40c/phone/battery AND /ndn/org/md2k/ATTRIBUTE/location/commuting))");
-    for (const auto &stream : result.streams) {
+    for (const std::string &stream : result.streams) {
         BOOST_CHECK(
                 stream == "/ndn/org/md2k/mguard/dd40c/phone/accelerometer" ||
                 stream == "/ndn/org/md2k/mguard/dd40c/phone/battery"
                 );
     }
-    for (const auto &requester : result.requesters) {
+    for (const std::string &requester : result.requesters) {
         BOOST_CHECK(requester == "/ndn/org/md2k/A");
     }
 }
