b51c45a5cd41586d6b1696cc1a301a261c47ca31
Adam Thieme <athieme@memphis.edu>
2022-12-13 22:20:26 -0600

    testing

--- tests/unit-tests/parser.t.cpp
+++ tests/unit-tests/parser.t.cpp
@@ -96,7 +96,7 @@ BOOST_AUTO_TEST_CASE(Constructor)
 
     // policy 5
     result = parser.parsePolicy("tests/unit-tests/parser-resources/policy5");
-    BOOST_CHECK(result.policyIdentifier == "5");
+    BOOST_CHECK(result.policyIdentifier == "4");
     BOOST_CHECK(result.abePolicy == "/ndn/org/md2k/mguard/dd40c/phone/accelerometer");
     for (const std::string &stream : result.streams) {
         BOOST_CHECK(
