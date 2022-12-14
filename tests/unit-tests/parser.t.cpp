4004ec011a14d9decbfb2dd91b8220e3a4735ec7
Adam Thieme <athieme@memphis.edu>
2022-12-13 21:39:24 -0600

    more tests

--- tests/unit-tests/parser.t.cpp
+++ tests/unit-tests/parser.t.cpp
@@ -15,6 +15,7 @@ BOOST_AUTO_TEST_CASE(Constructor)
     PolicyParser parser("tests/unit-tests/parser-resources/available_streams");
     PolicyDetail result;
 
+    // policy 1
     result = parser.parsePolicy("tests/unit-tests/parser-resources/policy1");
     BOOST_CHECK(result.policyIdentifier == "1");
     BOOST_CHECK(result.abePolicy == "((/ndn/org/md2k/mguard/dd40c/phone/accelerometer AND /ndn/org/md2k/ATTRIBUTE/location/gym) OR (/ndn/org/md2k/mguard/dd40c/phone/battery AND /ndn/org/md2k/ATTRIBUTE/location/commuting))");
@@ -28,8 +29,9 @@ BOOST_AUTO_TEST_CASE(Constructor)
         BOOST_CHECK(requester == "/ndn/org/md2k/A");
     }
 
+    // policy 2
     result = parser.parsePolicy("tests/unit-tests/parser-resources/policy2");
-    BOOST_CHECK(result.policyIdentifier == "1");
+    BOOST_CHECK(result.policyIdentifier == "2");
     BOOST_CHECK(result.abePolicy == "(/ndn/org/md2k/mguard/dd40c/phone/accelerometer OR /ndn/org/md2k/mguard/dd40c/phone/gyroscope OR /ndn/org/md2k/mguard/dd40c/phone/battery OR /ndn/org/md2k/mguard/dd40c/phone/gps OR /ndn/org/md2k/mguard/dd40c/data_analysis/gps_episodes_and_semantic_location)");
     for (const std::string &stream : result.streams) {
         BOOST_CHECK(
@@ -48,6 +50,90 @@ BOOST_AUTO_TEST_CASE(Constructor)
                 requester == "/ndn/org/md2k/D" ||
                 requester == "/ndn/org/md2k/E" ||
                 requester == "/ndn/org/md2k/F"
+        );
+    }
+
+    // policy 3
+    result = parser.parsePolicy("tests/unit-tests/parser-resources/policy3");
+    BOOST_CHECK(result.policyIdentifier == "3");
+    BOOST_CHECK(result.abePolicy == "(/ndn/org/md2k/mguard/dd40c/phone/battery)");
+    for (const std::string &stream : result.streams) {
+        BOOST_CHECK(
+                stream == "/ndn/org/md2k/mguard/dd40c/phone/battery"
+        );
+    }
+    for (const std::string &requester : result.requesters) {
+        BOOST_CHECK(
+                requester == "/ndn/org/md2k/A" ||
+                requester == "/ndn/org/md2k/B" ||
+                requester == "/ndn/org/md2k/C" ||
+                requester == "/ndn/org/md2k/D" ||
+                requester == "/ndn/org/md2k/E"
+        );
+    }
+
+    // policy 4
+    result = parser.parsePolicy("tests/unit-tests/parser-resources/policy4");
+    BOOST_CHECK(result.policyIdentifier == "4");
+    BOOST_CHECK(result.abePolicy == "(/ndn/org/md2k/mguard/dd40c/phone/accelerometer OR /ndn/org/md2k/mguard/dd40c/phone/gyroscope OR /ndn/org/md2k/mguard/dd40c/phone/battery OR /ndn/org/md2k/mguard/dd40c/phone/gps OR /ndn/org/md2k/mguard/dd40c/data_analysis/gps_episodes_and_semantic_location) AND (/ndn/org/md2k/ATTRIBUTE/location/work)");
+    for (const std::string &stream : result.streams) {
+        BOOST_CHECK(
+                stream == "/ndn/org/md2k/mguard/dd40c/phone/battery"
+        );
+    }
+    for (const std::string &requester : result.requesters) {
+        BOOST_CHECK(
+                requester == "/ndn/org/md2k/A" ||
+                requester == "/ndn/org/md2k/B" ||
+                requester == "/ndn/org/md2k/C" ||
+                requester == "/ndn/org/md2k/D" ||
+                requester == "/ndn/org/md2k/E"
+        );
+    }
+
+    // policy 5
+    result = parser.parsePolicy("tests/unit-tests/parser-resources/policy5");
+    BOOST_CHECK(result.policyIdentifier == "5");
+    BOOST_CHECK(result.abePolicy == "(/ndn/org/md2k/mguard/dd40c/phone/accelerometer)");
+    for (const std::string &stream : result.streams) {
+        BOOST_CHECK(
+                stream == "/ndn/org/md2k/mguard/dd40c/phone/accelerometer"
+        );
+    }
+    for (const std::string &requester : result.requesters) {
+        BOOST_CHECK(
+                requester == "/ndn/org/md2k/E" ||
+                requester == "/ndn/org/md2k/F"
+        );
+    }
+
+    // policy 6
+    // we do not have implementation for wildcards within stream names
+
+    // policy 7
+    // we do not have an accelerometer attribute
+
+    // policy 8
+    result = parser.parsePolicy("tests/unit-tests/parser-resources/policy8");
+    BOOST_CHECK(result.policyIdentifier == "8");
+    BOOST_CHECK(result.abePolicy == "(/ndn/org/md2k/mguard/dd40c/phone/accelerometer)");
+    for (const std::string &stream : result.streams) {
+        BOOST_CHECK(
+                stream == "/ndn/org/md2k/mguard/dd40c/phone/accelerometer" ||
+                stream == "/ndn/org/md2k/mguard/dd40c/data_analysis/gps_episodes_and_semantic_location" ||
+                stream == "/ndn/org/md2k/mguard/dd40c/phone/gyroscope" ||
+                stream == "/ndn/org/md2k/mguard/dd40c/phone/battery" ||
+                stream == "/ndn/org/md2k/mguard/dd40c/phone/gps"
+        );
+
+    }
+    for (const std::string &requester : result.requesters) {
+        BOOST_CHECK(
+                requester == "/ndn/org/md2k/A" ||
+                requester == "/ndn/org/md2k/B" ||
+                requester == "/ndn/org/md2k/C" ||
+                requester == "/ndn/org/md2k/D" ||
+                requester == "/ndn/org/md2k/E"
         );
     }
 
