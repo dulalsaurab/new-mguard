2e95293ed105c428790ecb371b21c7343779d0eb
Saurab Dulal <sdulal@memphis.edu>
2023-02-21 21:17:05 +0000

    add the ui content

--- examples/consumerApp.cpp
+++ examples/consumerApp.cpp
@@ -21,8 +21,15 @@
 #include <user/subscriber.hpp>
 #include <common.hpp>
 
+#include <UI/mywindow.cpp>
+
 #include <ndn-cxx/util/logger.hpp>
 #include <ndn-cxx/face.hpp>
+
+#include <gtkmm.h>
+#include <glib.h>
+#include <gtkmm/application.h>
+
 
 #include <string>
 #include <sstream>
@@ -33,179 +40,199 @@
 #include <boost/program_options/parsers.hpp>
 
 using namespace ndn::time_literals;
+using namespace Glib;
+using namespace Gtk;
 
-NDN_LOG_INIT(mguard.examples.consumerApp);
 
-static void
-usage(const boost::program_options::options_description& options)
-{
-  NDN_LOG_INFO("Usage: ndnsd-consumer [options] e.g. printer \n" << options);
-   exit(2);
-}
+// NDN_LOG_INIT(mguard.examples.consumerApp);
 
-class mGuardConsumer
-{
-public:
+// static void
+// usage(const boost::program_options::options_description& options)
+// {
+//   NDN_LOG_INFO("Usage: ndnsd-consumer [options] e.g. printer \n" << options);
+//    exit(2);
+// }
 
- mGuardConsumer(ndn::Name& consumerPrefix, ndn::Name& syncPrefix, ndn::Name& controllerPrefix,
-                std::string& consumerCertPath, std::string& aaCertPath)
- : m_subscriber(consumerPrefix, syncPrefix, controllerPrefix,
-                 consumerCertPath, aaCertPath, 1000_ms,
-                 std::bind(&mGuardConsumer::processDataCallback, this, _1),
-                 std::bind(&mGuardConsumer::processSubscriptionCallback, this, _1))
-  {
-  }
+// class mGuardConsumer
+// {
+// public:
 
-  void
-  processDataCallback(const std::map<std::string, std::string>& updates)
-  {
-    for (auto const& x : updates)
-    {
-      NDN_LOG_INFO("Received data: " << x.second << " for name: " << x.first);
-    }
-  }
+//  mGuardConsumer(ndn::Name& consumerPrefix, ndn::Name& syncPrefix, ndn::Name& controllerPrefix,
+//                 std::string& consumerCertPath, std::string& aaCertPath)
+//  : m_subscriber(consumerPrefix, syncPrefix, controllerPrefix,
+//                  consumerCertPath, aaCertPath, 1000_ms,
+//                  std::bind(&mGuardConsumer::processDataCallback, this, _1),
+//                  std::bind(&mGuardConsumer::processSubscriptionCallback, this, _1))
+//   {
+//   }
 
-  void
-  processSubscriptionCallback(const std::unordered_set<ndn::Name>& streams)
-  {
-     // wait until keys are fetched?
-    // check for convergence.
-    if (!m_subscriber.checkConvergence()) {
-      NDN_LOG_DEBUG("couldnt fetch appropriate keys, exiting");
-      exit(-1);
-    }
+//   void
+//   processDataCallback(const std::map<std::string, std::string>& updates)
+//   {
+//     for (auto const& x : updates)
+//     {
+//       NDN_LOG_INFO("Received data: " << x.second << " for name: " << x.first);
+//     }
+//   }
 
-    NDN_LOG_INFO("\n\nStreams available for subscription");
-    std::vector<ndn::Name> availableStreams, subscriptionList;
+//   void
+//   processSubscriptionCallback(const std::unordered_set<ndn::Name>& streams)
+//   {
+//      // wait until keys are fetched?
+//     // check for convergence.
+//     if (!m_subscriber.checkConvergence()) {
+//       NDN_LOG_DEBUG("couldnt fetch appropriate keys, exiting");
+//       exit(-1);
+//     }
+
+//     NDN_LOG_INFO("\n\nStreams available for subscription");
+//     std::vector<ndn::Name> availableStreams, subscriptionList;
   
-    int counter=0;
-    if (streams.size() <= 0) {
-      NDN_LOG_INFO("No eligible stream found for your policy");
-    }
+//     int counter=0;
+//     if (streams.size() <= 0) {
+//       NDN_LOG_INFO("No eligible stream found for your policy");
+//     }
 
-    for (auto &a : streams) {
-      NDN_LOG_INFO(++counter << ": " << a);
-      availableStreams.push_back(a);
-    }
+//     for (auto &a : streams) {
+//       NDN_LOG_INFO(++counter << ": " << a);
+//       availableStreams.push_back(a);
+//     }
 
-    // these codes are only for testing purposes
-    // automatically subscriber to the respective streams
+//     // these codes are only for testing purposes
+//     // automatically subscriber to the respective streams
 
-    // A. battery only 
-    // subscriptionList.push_back(availableStreams[0]); // battery
+//     // A. battery only 
+//     // subscriptionList.push_back(availableStreams[0]); // battery
 
-    // all stream
-    subscriptionList.push_back(availableStreams[0]); // battery
-    subscriptionList.push_back(availableStreams[1]); // semloc
-    subscriptionList.push_back(availableStreams[3]); // gps
+//     // all stream
+//     subscriptionList.push_back(availableStreams[0]); // battery
+//     subscriptionList.push_back(availableStreams[1]); // semloc
+//     subscriptionList.push_back(availableStreams[3]); // gps
 
-    // not gps
-    // subscriptionList.push_back(availableStreams[0]); // battery
-    // subscriptionList.push_back(availableStreams[2]); // sem_loc
+//     // not gps
+//     // subscriptionList.push_back(availableStreams[0]); // battery
+//     // subscriptionList.push_back(availableStreams[2]); // sem_loc
 
-    // accelerometer
-    // subscriptionList.push_back(availableStreams[0]); // battery
+//     // accelerometer
+//     // subscriptionList.push_back(availableStreams[0]); // battery
 
-    // only work
-    // subscriptionList.push_back(availableStreams[0]); // gps, only the one with attribute work should be accessible
+//     // only work
+//     // subscriptionList.push_back(availableStreams[0]); // gps, only the one with attribute work should be accessible
 
-    for (auto& s: subscriptionList) {
-      // m_subscriber.subscribe(s);
-      NDN_LOG_DEBUG("Subscribed to the stream/s" << s); // << std::endl;
-    }
+//     for (auto& s: subscriptionList) {
+//       // m_subscriber.subscribe(s);
+//       NDN_LOG_DEBUG("Subscribed to the stream/s" << s); // << std::endl;
+//     }
 
 
-    // uncomment if: taking input from user ----------------------------------------------
+//     // uncomment if: taking input from user ----------------------------------------------
     
-    // std::vector<int> input; //
-    // std::cout << "enter selection, enter any char to stop" << std::endl;
-    // while(!std::cin.fail())
-    // {
-    //     int value;
-    //     std::cin >> value;
-    //     if(!std::cin.fail())
-    //       input.push_back(value);
-    // }
-    // std::cout << "\n" << std::endl;
-    // std::cout << "Subscribed to the stream/s" << std::endl;
-    // for (auto k : input)
-    // {
-    //   auto ind = k-1;
-    //   std::cout << k << ": " << availableStreams[ind] << std::endl;
-    //   if (availableStreams[ind] != "/") // todo: fix this
-    //     subscriptionList.push_back(availableStreams[ind]);
-    // }
+//     // std::vector<int> input; //
+//     // std::cout << "enter selection, enter any char to stop" << std::endl;
+//     // while(!std::cin.fail())
+//     // {
+//     //     int value;
+//     //     std::cin >> value;
+//     //     if(!std::cin.fail())
+//     //       input.push_back(value);
+//     // }
+//     // std::cout << "\n" << std::endl;
+//     // std::cout << "Subscribed to the stream/s" << std::endl;
+//     // for (auto k : input)
+//     // {
+//     //   auto ind = k-1;
+//     //   std::cout << k << ": " << availableStreams[ind] << std::endl;
+//     //   if (availableStreams[ind] != "/") // todo: fix this
+//     //     subscriptionList.push_back(availableStreams[ind]);
+//     // }
    
-    // taking input from user end----------------------------------------------
+//     // taking input from user end----------------------------------------------
 
-    m_subscriber.setSubscriptionList(subscriptionList);
+//     m_subscriber.setSubscriptionList(subscriptionList);
     
-    NDN_LOG_DEBUG("---------");
-    for(auto x: m_subscriber.getSubscriptionList())
-      NDN_LOG_DEBUG("stream: "<< x);
+//     NDN_LOG_DEBUG("---------");
+//     for(auto x: m_subscriber.getSubscriptionList())
+//       NDN_LOG_DEBUG("stream: "<< x);
 
-    m_subscriber.unsubscribe(availableStreams[3]);
+//     m_subscriber.unsubscribe(availableStreams[3]);
     
-    NDN_LOG_DEBUG("---------");
-    for(auto x: m_subscriber.getSubscriptionList())
-      NDN_LOG_DEBUG("stream: "<< x);
-  }
+//     NDN_LOG_DEBUG("---------");
+//     for(auto x: m_subscriber.getSubscriptionList())
+//       NDN_LOG_DEBUG("stream: "<< x);
+//   }
 
-  void
-  handler() { m_subscriber.run(); }
+//   void
+//   handler() { m_subscriber.run(); }
 
-private:
-  ndn::Face m_face;
-  mguard::subscriber::Subscriber m_subscriber;
-};
+// private:
+//   ndn::Face m_face;
+//   mguard::subscriber::Subscriber m_subscriber;
+// };
 
-int
-main(int argc, char* argv[])
-{
-  std::string applicationPrefix;
-  std::string certPath;
+// int
+// main(int argc, char* argv[])
+// {
+//   std::string applicationPrefix;
+//   std::string certPath;
 
-  namespace po = boost::program_options;
-  po::options_description visibleOptDesc("Options");
+//   namespace po = boost::program_options;
+//   po::options_description visibleOptDesc("Options");
 
-  visibleOptDesc.add_options()
-    ("help,h",      "print this message and exit")
-    ("applicationPrefix,p", po::value<std::string>(&applicationPrefix)->required(), "application prefix, this name needs to match the one controller has")
-    ("certificatePath,c", po::value<std::string>(&certPath), " location of consumer certificate")
-  ;
+//   visibleOptDesc.add_options()
+//     ("help,h",      "print this message and exit")
+//     ("applicationPrefix,p", po::value<std::string>(&applicationPrefix)->required(), "application prefix, this name needs to match the one controller has")
+//     ("certificatePath,c", po::value<std::string>(&certPath), " location of consumer certificate")
+//   ;
   
-  try
-  {
-    po::variables_map optVm;
-    po::store(po::parse_command_line(argc, argv, visibleOptDesc), optVm);
-    po::notify(optVm);
+//   try
+//   {
+//     po::variables_map optVm;
+//     po::store(po::parse_command_line(argc, argv, visibleOptDesc), optVm);
+//     po::notify(optVm);
 
-    if (optVm.count("applicationPrefix")) {
-      if (applicationPrefix.empty())
-      {
-        std::cerr << "ERROR: applicationPrefix cannot be empty" << std::endl;
-        usage(visibleOptDesc);
-      }
-    }
-    if (optVm.count("certificatePath")) {
-      if (certPath.empty())
-      {
-        std::cerr << "ERROR: certificatePath cannot be empty" << std::endl;
-        usage(visibleOptDesc);
-      }
-    }
+//     if (optVm.count("applicationPrefix")) {
+//       if (applicationPrefix.empty())
+//       {
+//         std::cerr << "ERROR: applicationPrefix cannot be empty" << std::endl;
+//         usage(visibleOptDesc);
+//       }
+//     }
+//     if (optVm.count("certificatePath")) {
+//       if (certPath.empty())
+//       {
+//         std::cerr << "ERROR: certificatePath cannot be empty" << std::endl;
+//         usage(visibleOptDesc);
+//       }
+//     }
 
-  }
-  catch (const po::error& e) {
-    std::cerr << "ERROR: " << e.what() << std::endl;
-    usage(visibleOptDesc);
-  }
+//   }
+//   catch (const po::error& e) {
+//     std::cerr << "ERROR: " << e.what() << std::endl;
+//     usage(visibleOptDesc);
+//   }
 
-  ndn::Name consumerPrefix(applicationPrefix);
+//   ndn::Name consumerPrefix(applicationPrefix);
+//   ndn::Name syncPrefix = "/ndn/org/md2k";
+//   ndn::Name controllerPrefix = "/ndn/org/md2k/mguard/controller";
+//   // std::string consumerCertPath = "certs/A.cert";
+//   std::string aaCertPath = "certs/aa.cert";
+//   mGuardConsumer consumer (consumerPrefix, syncPrefix, controllerPrefix, certPath, aaCertPath);
+//   consumer.handler();
+// }
+
+
+int main(int argc, char *argv[])
+{
+  ndn::Name consumerPrefix = "/ndn/org/md2k/A";
   ndn::Name syncPrefix = "/ndn/org/md2k";
   ndn::Name controllerPrefix = "/ndn/org/md2k/mguard/controller";
-  // std::string consumerCertPath = "certs/A.cert";
+  std::string consumerCertPath = "certs/A.cert";
+  std::string certPath = "certs/A.cert";
   std::string aaCertPath = "certs/aa.cert";
-  mGuardConsumer consumer (consumerPrefix, syncPrefix, controllerPrefix, certPath, aaCertPath);
-  consumer.handler();
+  // mGuardConsumer consumer (consumerPrefix, syncPrefix, controllerPrefix, certPath, aaCertPath);
+
+  RefPtr<Application> app = Application::create(argc, argv);
+  mywindow window(consumerPrefix, syncPrefix, controllerPrefix, certPath, aaCertPath);
+  return app->run(window);
+  // consumer.handler();
 }
