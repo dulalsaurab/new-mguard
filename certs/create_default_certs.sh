d67db59447b69ef8362ce2bdf834d7e97bf07e36
Adam Thieme <athieme@memphis.edu>
2023-07-05 16:53:44 -0500

    fixes for security namespace being off

---
+++ certs/create_default_certs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+server_certs() {
+ echo "Installing server certs (producer, controller, and attribute authority)"
+ ndnsec key-gen /ndn/org/md2k
+ ndnsec key-gen /ndn/org/md2k/mguard/controller
+ ndnsec key-gen /ndn/org/md2k/mguard/aa
+
+ ndnsec sign-req /ndn/org/md2k/mguard/controller > controller.ndncsr
+ ndnsec sign-req /ndn/org/md2k/mguard/aa > aa.ndncsr
+
+ ndnsec cert-gen -s /ndn/org/md2k -i /ndn/org/md2k aa.ndncsr > aa.cert
+ ndnsec cert-gen -s /ndn/org/md2k -i /ndn/org/md2k controller.ndncsr > controller.cert
+
+ ndnsec cert-dump -i /ndn/org/md2k > producer.cert
+ ndnsec cert-dump -i /ndn/org/md2k > md2k-trust-anchor.ndncert
+}
+
+consumer_cert() {
+  echo "Creating and installing consumer certificate"
+  ndnsec key-gen /ndn/org/md2k/local
+  ndnsec sign-req /ndn/org/md2k/local > local.ndncsr
+  ndnsec cert-gen -s /ndn/org/md2k -i /ndn/org/md2k local.ndncsr > local.cert
+}
+
+if [ "$1" == "-a" ]; then
+  server_certs
+  sleep 1
+  consumer_cert
+  exit
+fi
+
+server_certs
+
+
--- runner.sh
+++ runner.sh
@@ -4,9 +4,6 @@ export NDN_LOG=ndn.security.*=DEBUG:mguard.*=DEBUG:nacabe.*=DEBUG
 echo "reset debug logs"
 rm -rf debug
 mkdir debug
-
-sleep 2
-
 sudo ldconfig
 
 #sudo pkill -f -e -c mguard
@@ -23,36 +20,49 @@ nfdc cs erase /ndn/org/md2k
 echo "nfdstop"
 nfd-stop
 
-sleep 2
+sleep 5
 
 echo "delete repo"
 rm -rf ~/.ndn/ndn-python-repo/sqlite3.db
 
+echo "delete certs"
+ndnsec delete /ndn/org/md2k
+ndnsec delete /ndn/org/md2k/local
+ndnsec delete /ndn/org/md2k/mguard
+ndnsec delete /ndn/org/md2k/mguard/aa
+ndnsec delete /ndn/org/md2k/mguard/controller
+
+echo "create certs"
+#cd certs && ./create_default_certs.sh -a && cd ..
+cd certs && ./create_certs.sh -a && cd ..
+rm -rf ../consoomer/cmake-build-debug/certs && cp -r certs ../consoomer/cmake-build-debug/
+#cd certs && bash create_default_certs.sh && cd ..
+
 echo "start nfd"
 sudo nfd-start &>/dev/null
 
-sleep 1
+sleep 5
 
 echo "start repo"
 ndn-python-repo -c ndn-python-repo.conf > debug/repo.log 2>&1 &
 
-sleep 1
+sleep 5
 
 echo "start controller"
 mguard-controllerApp > debug/controller.log 2>&1 &
 
-sleep 1
+sleep 5
 
 # valgrind --leak-check=full --log-file=valgrind-out.txt ./build/examples/mguard-producerApp > debugging/p.log 2>&1 &
 echo "start producer"
 mguard-producerApp > debug/producer.log 2>&1 &
 
-sleep 4
+sleep 7
 
 echo "run data generator"
 python data-generator/CerebralCortex-Random-Data-Generator/main.py > debug/generator.log 2>&1 &
 
-sleep 0.1
+sleep 5
 
 echo "run consumer"
 mguard-consumerApp -c certs/local.cert -p /ndn/org/md2k/local > debug/consumer.log 2>&1 &
